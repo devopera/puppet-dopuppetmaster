8c522a14fb28c534c894a7dd5183e80fd866fbd7
cleverlight <github.account@cleveraspaint.com>
2013-11-05 22:00:06 +0000

    moved services over to fqdn

--- manifests/monitor.pp
+++ manifests/monitor.pp
@@ -10,10 +10,10 @@ class dopuppetmaster::monitor (
 
 ) {
 
-  @nagios::service { "int:process_puppetmaster-dopuppetmaster-${::hostname}":
+  @nagios::service { "int:process_puppetmaster-dopuppetmaster-${::fqdn}":
     check_command => "check_procs!1:!1:!'puppet master'",
   }
-  @nagios::service { "int:process_puppetdb-dopuppetmaster-${::hostname}":
+  @nagios::service { "int:process_puppetdb-dopuppetmaster-${::fqdn}":
     check_command => "check_procs!1:!1:!puppetdb",
   }
 
